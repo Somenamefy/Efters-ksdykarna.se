251163ac4237937113ec216f3d2212c242a9f93a
Somenamefy <97879935+Somenamefy@users.noreply.github.com>
2023-05-15 16:21:13 +0200

    byggt till lite

---
+++ script.js
@@ -0,0 +1,30 @@
+document.querySelector('#hund').addEventListener('click', () => {
+  let contentHeader = document.querySelector('#contentHeader');
+  let contentText = document.querySelector('#contentText');
+  contentHeader.innerText = "Eftersök med hund";
+  contentText.innerText = "Vi har hundförare vars hundar tränats för eftersök på människor. Dessa hundar är väldigt effektiva vid de tillfällen då objektet skulle vara avlidet.";
+});
+document.querySelector('#drönare').addEventListener('click', () => {
+  let contentHeader = document.querySelector('#contentHeader');
+  let contentText = document.querySelector('#contentText');
+  contentHeader.innerText = "Eftersök med drönare";
+  contentText.innerText = "Vi har tillgång till luft- och Undervattens-drönare. Detta skapar bra förutsättningar för en lyckad insats då vi kan genomsöka området både från luft och under ytan.";
+});
+document.querySelector('#båt').addEventListener('click', () => {
+  let contentHeader = document.querySelector('#contentHeader');
+  let contentText = document.querySelector('#contentText');
+  contentHeader.innerText = "Eftersök med båt";
+  contentText.innerText = "Vi har tillgång till båtar som kan användas vid insatser när objektet inte skulle kunna hittas från land. Vi kan även scanna botten för att kunna hitta sökområden innan vi tillsätter mer resurser.";
+});
+document.querySelector('#dyk').addEventListener('click', () => {
+  let contentHeader = document.querySelector('#contentHeader');
+  let contentText = document.querySelector('#contentText');
+  contentHeader.innerText = "Eftersök med dykare";
+  contentText.innerText = "Vi har proffsiga dykare, med mycket erfarenhet, som kan agera sök-element under ytan, samt analysera strömmar. Att använda dykare vid insatser är en grundförutsättning för en lyckad insats då objektet misstänks befinna sig under ytan.";
+});
+document.querySelector('#logga').addEventListener('click', () => {
+  let contentHeader = document.querySelector('#contentHeader');
+  let contentText = document.querySelector('#contentText');
+  contentHeader.innerText = "Vi är Eftersöksdykarna";
+  contentText.innerText = "Eftersöksdykarna är en idiell organisation som strävar efter spets kunskap inom sök i vattendrag, Vi strävar efter samarbete mellan myndigheter och privat personer för att kunna hjälpa hitta nära och kära som är försvunna.";
+});
